1e31af2ed915f01f054ca351ae21289eeb4d4bb4
Shashank Daima <daimashashank10@gmail.com>
2023-06-17 02:56:33 +0530

    created custom user with user manager

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+from .user_manager import UserManager
 # Create your models here.
+class User(AbstractUser):
+    created_date = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(null=True, blank=True)
+    is_banned = models.BooleanField(default=False)
+    password = models.CharField(max_length=128)
+    email = models.CharField(max_length=256,unique=True )
+    username=None
+
+    USERNAME_FIELD="email"
+    REQUIRED_FIELDS=["password"]
+
+    objects=UserManager()
+
+    def __str__(self):
+        return self.email
+    
